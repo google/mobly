a034e94a68095d25027e3c5aaf59918c2b4a4df0
Ang Li <angli@google.com>
2017-01-31 17:57:18 -0800

    Use tr to trim the damn carriage linebreaks.

--- mobly/controllers/android_device_lib/snippet_client.py
+++ mobly/controllers/android_device_lib/snippet_client.py
@@ -80,25 +80,27 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
         """Overrides superclass."""
         # Check that the Mobly Snippet app is installed.
         if not self._adb_grep_wrapper(
-                'pm list package | grep "^package:%s\\r\?$"' % self.package):
+                'pm list package | tr -d "\\r" | grep "^package:%s$"' %
+                self.package):
             raise jsonrpc_client_base.AppStartError(
                 '%s is not installed on %s' % (self.package, self._serial))
         # Check that the app is instrumented.
         out = self._adb_grep_wrapper(
-            'pm list instrumentation | grep ^instrumentation:%s/%s' %
-            (self.package, _INSTRUMENTATION_RUNNER_PACKAGE))
+            'pm list instrumentation | tr -d "\\r" | grep ^instrumentation:%s/%s'
+            % (self.package, _INSTRUMENTATION_RUNNER_PACKAGE))
         if not out:
             raise jsonrpc_client_base.AppStartError(
                 '%s is installed on %s, but it is not instrumented.' %
                 (self.package, self._serial))
-        match = re.search(r'^instrumentation:(.*)\/(.*) \(target=(.*)\)\r?$',
+        match = re.search(r'^instrumentation:(.*)\/(.*) \(target=(.*)\)$',
                           out)
         target_name = match.group(3)
         # Check that the instrumentation target is installed if it's not the
         # same as the snippet package.
         if target_name != self.package:
             out = self._adb_grep_wrapper(
-                'pm list package | grep ^package:%s$' % target_name)
+                'pm list package | tr -d "\\r" | grep ^package:%s$' %
+                target_name)
             if not out:
                 raise jsonrpc_client_base.AppStartError(
                     'Instrumentation target %s is not installed on %s' %
