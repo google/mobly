fc99e8f2e170f26574a8d95f921943f200f6d5dc
Ang Li <angli@google.com>
2017-01-31 18:04:38 -0800

    Use raw string.

--- mobly/controllers/android_device_lib/snippet_client.py
+++ mobly/controllers/android_device_lib/snippet_client.py
@@ -80,13 +80,13 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
         """Overrides superclass."""
         # Check that the Mobly Snippet app is installed.
         if not self._adb_grep_wrapper(
-                'pm list package | tr -d "\\r" | grep "^package:%s$"' %
+                r'pm list package | tr -d "\r" | grep "^package:%s$"' %
                 self.package):
             raise jsonrpc_client_base.AppStartError(
                 '%s is not installed on %s' % (self.package, self._serial))
         # Check that the app is instrumented.
         out = self._adb_grep_wrapper(
-            'pm list instrumentation | tr -d "\\r" | grep ^instrumentation:%s/%s'
+            r'pm list instrumentation | tr -d "\r" | grep ^instrumentation:%s/%s'
             % (self.package, _INSTRUMENTATION_RUNNER_PACKAGE))
         if not out:
             raise jsonrpc_client_base.AppStartError(
@@ -99,7 +99,7 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
         # same as the snippet package.
         if target_name != self.package:
             out = self._adb_grep_wrapper(
-                'pm list package | tr -d "\\r" | grep ^package:%s$' %
+                r'pm list package | tr -d "\r" | grep ^package:%s$' %
                 target_name)
             if not out:
                 raise jsonrpc_client_base.AppStartError(
