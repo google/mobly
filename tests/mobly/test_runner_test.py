ca9398890d73923f12c2251af296601d00ed30a7
Ang Li <angli@google.com>
2017-03-06 23:15:43 -0800

    Use single quote in test runner test.

--- tests/mobly/test_runner_test.py
+++ tests/mobly/test_runner_test.py
@@ -43,12 +43,12 @@ class TestRunnerTest(unittest.TestCase):
     def setUp(self):
         self.tmp_dir = tempfile.mkdtemp()
         self.base_mock_test_config = config_parser.TestRunConfig()
-        self.base_mock_test_config.test_bed_name = "SampleTestBed"
+        self.base_mock_test_config.test_bed_name = 'SampleTestBed'
         self.base_mock_test_config.controller_configs = {}
         self.base_mock_test_config.user_params = {
-                "cli_args": None,
-                "icecream": 42,
-                "extra_param": "haha"
+                'cli_args': None,
+                'icecream': 42,
+                'extra_param': 'haha'
             }
         self.base_mock_test_config.log_path = self.tmp_dir
         self.mock_run_list = [('SampleTest', None)]
@@ -60,7 +60,7 @@ class TestRunnerTest(unittest.TestCase):
         tr = test_runner.TestRunner(self.base_mock_test_config,
                                     self.mock_run_list)
         with self.assertRaisesRegexp(signals.ControllerError,
-                                     "No corresponding config found for"):
+                                     'No corresponding config found for'):
             tr.register_controller(mock_controller)
 
     def test_register_controller_no_config_no_register(self):
@@ -77,55 +77,55 @@ class TestRunnerTest(unittest.TestCase):
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
         mock_test_config.controller_configs = {
-            mock_ctrlr_config_name: ["magic1", "magic2"]
+            mock_ctrlr_config_name: ['magic1', 'magic2']
         }
         tr = test_runner.TestRunner(mock_test_config, self.mock_run_list)
         tr.register_controller(mock_controller)
-        registered_name = "mock_controller"
+        registered_name = 'mock_controller'
         self.assertTrue(registered_name in tr.controller_registry)
         mock_ctrlrs = tr.controller_registry[registered_name]
-        self.assertEqual(mock_ctrlrs[0].magic, "magic1")
-        self.assertEqual(mock_ctrlrs[1].magic, "magic2")
+        self.assertEqual(mock_ctrlrs[0].magic, 'magic1')
+        self.assertEqual(mock_ctrlrs[1].magic, 'magic2')
         self.assertTrue(tr.controller_destructors[registered_name])
-        expected_msg = "Controller module .* has already been registered."
+        expected_msg = 'Controller module .* has already been registered.'
         with self.assertRaisesRegexp(signals.ControllerError, expected_msg):
             tr.register_controller(mock_controller)
 
     def test_register_controller_no_get_info(self):
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
-        mock_ref_name = "haha"
-        get_info = getattr(mock_controller, "get_info")
-        delattr(mock_controller, "get_info")
+        mock_ref_name = 'haha'
+        get_info = getattr(mock_controller, 'get_info')
+        delattr(mock_controller, 'get_info')
         try:
             mock_test_config.controller_configs = {
-                mock_ctrlr_config_name: ["magic1", "magic2"]
+                mock_ctrlr_config_name: ['magic1', 'magic2']
             }
             tr = test_runner.TestRunner(mock_test_config, self.mock_run_list)
             tr.register_controller(mock_controller)
             self.assertEqual(tr.results.controller_info, {})
         finally:
-            setattr(mock_controller, "get_info", get_info)
+            setattr(mock_controller, 'get_info', get_info)
 
     def test_register_controller_return_value(self):
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
         mock_test_config.controller_configs = {
-            mock_ctrlr_config_name: ["magic1", "magic2"]
+            mock_ctrlr_config_name: ['magic1', 'magic2']
         }
         tr = test_runner.TestRunner(mock_test_config, self.mock_run_list)
         magic_devices = tr.register_controller(mock_controller)
-        self.assertEqual(magic_devices[0].magic, "magic1")
-        self.assertEqual(magic_devices[1].magic, "magic2")
+        self.assertEqual(magic_devices[0].magic, 'magic1')
+        self.assertEqual(magic_devices[1].magic, 'magic2')
 
     def test_register_controller_less_than_min_number(self):
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
         mock_test_config.controller_configs = {
-            mock_ctrlr_config_name: ["magic1", "magic2"]
+            mock_ctrlr_config_name: ['magic1', 'magic2']
         }
         tr = test_runner.TestRunner(mock_test_config, self.mock_run_list)
-        expected_msg = "Expected to get at least 3 controller objects, got 2."
+        expected_msg = 'Expected to get at least 3 controller objects, got 2.'
         with self.assertRaisesRegexp(signals.ControllerError, expected_msg):
             tr.register_controller(mock_controller, min_number=3)
 
@@ -137,9 +137,9 @@ class TestRunnerTest(unittest.TestCase):
         """
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
-        my_config = [{"serial": "xxxx",
-                      "magic": "Magic1"}, {"serial": "xxxx",
-                                           "magic": "Magic2"}]
+        my_config = [{'serial': 'xxxx',
+                      'magic': 'Magic1'}, {'serial': 'xxxx',
+                                           'magic': 'Magic2'}]
         mock_test_config.controller_configs[mock_ctrlr_config_name] = my_config
         tr = test_runner.TestRunner(mock_test_config, [('IntegrationTest',
                                                         None)])
@@ -152,9 +152,9 @@ class TestRunnerTest(unittest.TestCase):
         self.assertFalse(tr.controller_registry)
         self.assertFalse(tr.controller_destructors)
         results = tr.results.summary_dict()
-        self.assertEqual(results["Requested"], 2)
-        self.assertEqual(results["Executed"], 2)
-        self.assertEqual(results["Passed"], 2)
+        self.assertEqual(results['Requested'], 2)
+        self.assertEqual(results['Executed'], 2)
+        self.assertEqual(results['Passed'], 2)
         expected_info = {'MagicDevice': [{'MyMagic': {'magic': 'Magic1'}},
                                          {'MyMagic': {'magic': 'Magic2'}}]}
         self.assertEqual(tr.results.controller_info, expected_info)
@@ -164,7 +164,7 @@ class TestRunnerTest(unittest.TestCase):
     @mock.patch('mobly.controllers.android_device_lib.fastboot.FastbootProxy',
                 return_value=mock_android_device.MockFastbootProxy(1))
     @mock.patch('mobly.controllers.android_device.list_adb_devices',
-                return_value=["1"])
+                return_value=['1'])
     @mock.patch('mobly.controllers.android_device.get_all_instances',
                 return_value=mock_android_device.get_mock_ads(1))
     def test_run_two_test_classes(self, mock_get_all, mock_list_adb,
@@ -177,11 +177,11 @@ class TestRunnerTest(unittest.TestCase):
         """
         mock_test_config = self.base_mock_test_config.copy()
         mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
-        my_config = [{"serial": "xxxx", "magic": "Magic1"},
-                     {"serial": "xxxx", "magic": "Magic2"}]
+        my_config = [{'serial': 'xxxx', 'magic': 'Magic1'},
+                     {'serial': 'xxxx', 'magic': 'Magic2'}]
         mock_test_config.controller_configs[mock_ctrlr_config_name] = my_config
-        mock_test_config.controller_configs["AndroidDevice"] = [
-            {"serial": "1"}
+        mock_test_config.controller_configs['AndroidDevice'] = [
+            {'serial': '1'}
         ]
         tr = test_runner.TestRunner(mock_test_config,
                                     [('Integration2Test', None),
@@ -192,9 +192,9 @@ class TestRunnerTest(unittest.TestCase):
         self.assertFalse(tr.controller_registry)
         self.assertFalse(tr.controller_destructors)
         results = tr.results.summary_dict()
-        self.assertEqual(results["Requested"], 2)
-        self.assertEqual(results["Executed"], 2)
-        self.assertEqual(results["Passed"], 2)
+        self.assertEqual(results['Requested'], 2)
+        self.assertEqual(results['Executed'], 2)
+        self.assertEqual(results['Passed'], 2)
 
     def test_verify_controller_module(self):
         test_runner.TestRunner.verify_controller_module(mock_controller)
@@ -203,7 +203,7 @@ class TestRunnerTest(unittest.TestCase):
         try:
             tmp = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
             mock_controller.MOBLY_CONTROLLER_CONFIG_NAME = None
-            msg = "Controller interface .* in .* cannot be null."
+            msg = 'Controller interface .* in .* cannot be null.'
             with self.assertRaisesRegexp(signals.ControllerError, msg):
                 test_runner.TestRunner.verify_controller_module(
                     mock_controller)
@@ -213,13 +213,13 @@ class TestRunnerTest(unittest.TestCase):
     def test_verify_controller_module_missing_attr(self):
         try:
             tmp = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
-            delattr(mock_controller, "MOBLY_CONTROLLER_CONFIG_NAME")
-            msg = "Module .* missing required controller module attribute"
+            delattr(mock_controller, 'MOBLY_CONTROLLER_CONFIG_NAME')
+            msg = 'Module .* missing required controller module attribute'
             with self.assertRaisesRegexp(signals.ControllerError, msg):
                 test_runner.TestRunner.verify_controller_module(
                     mock_controller)
         finally:
-            setattr(mock_controller, "MOBLY_CONTROLLER_CONFIG_NAME", tmp)
+            setattr(mock_controller, 'MOBLY_CONTROLLER_CONFIG_NAME', tmp)
 
 
 if __name__ == "__main__":
