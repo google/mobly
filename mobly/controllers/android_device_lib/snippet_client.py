fbf87efbf06ec04007798c05b331787bd5153365
Ang Li <angli@google.com>
2017-04-03 22:45:15 -0700

    Fix snippet client for NBU devices. (#175)
    * Replace `grep` with in-Python regex matching.

--- mobly/controllers/android_device_lib/snippet_client.py
+++ mobly/controllers/android_device_lib/snippet_client.py
@@ -23,9 +23,8 @@ from mobly.controllers.android_device_lib import jsonrpc_client_base
 _INSTRUMENTATION_RUNNER_PACKAGE = (
     'com.google.android.mobly.snippet.SnippetRunner')
 
-_LAUNCH_CMD = (
-    'am instrument -w -e action start -e port %s %s/' +
-    _INSTRUMENTATION_RUNNER_PACKAGE)
+_LAUNCH_CMD = ('am instrument -w -e action start -e port %s %s/' +
+               _INSTRUMENTATION_RUNNER_PACKAGE)
 
 _STOP_CMD = (
     'am instrument -w -e action stop %s/' + _INSTRUMENTATION_RUNNER_PACKAGE)
@@ -83,7 +82,7 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
         # print the failure stack trace if there was any, and reap it from the
         # process table.
         if self._proc:
-          utils.stop_standing_subprocess(self._proc)
+            utils.stop_standing_subprocess(self._proc)
         self.log.debug('Stopping snippet apk %s', self.package)
         out = self._adb.shell(_STOP_CMD % self.package).decode('utf-8')
         if 'OK (0 tests)' not in out:
@@ -93,28 +92,26 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
     def check_app_installed(self):
         """Overrides superclass."""
         # Check that the Mobly Snippet app is installed.
-        if not self._adb_grep_wrapper(
-                r'pm list package | tr -d "\r" | grep "^package:%s$"' %
-                self.package):
+        out = self._adb.shell('pm list package')
+        if not self._grep('^package:%s$' % self.package, out):
             raise jsonrpc_client_base.AppStartError(
                 '%s is not installed on %s' % (self.package, self._serial))
         # Check that the app is instrumented.
-        out = self._adb_grep_wrapper(
-            r'pm list instrumentation | tr -d "\r" | grep ^instrumentation:%s/%s'
-            % (self.package, _INSTRUMENTATION_RUNNER_PACKAGE))
-        if not out:
+        out = self._adb.shell('pm list instrumentation')
+        matched_out = self._grep('^instrumentation:%s/%s' % (
+            self.package, _INSTRUMENTATION_RUNNER_PACKAGE), out)
+        if not matched_out:
             raise jsonrpc_client_base.AppStartError(
                 '%s is installed on %s, but it is not instrumented.' %
                 (self.package, self._serial))
-        match = re.search(r'^instrumentation:(.*)\/(.*) \(target=(.*)\)$', out)
+        match = re.search('^instrumentation:(.*)\/(.*) \(target=(.*)\)$',
+                          matched_out[0])
         target_name = match.group(3)
         # Check that the instrumentation target is installed if it's not the
         # same as the snippet package.
         if target_name != self.package:
-            out = self._adb_grep_wrapper(
-                r'pm list package | tr -d "\r" | grep ^package:%s$' %
-                target_name)
-            if not out:
+            out = self._adb.shell('pm list package')
+            if not self._grep('^package:%s$' % target_name, out):
                 raise jsonrpc_client_base.AppStartError(
                     'Instrumentation target %s is not installed on %s' %
                     (target_name, self._serial))
