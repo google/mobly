690269521c565c0de785a7a6ae71d766cea5b81d
David T.H. Kao <dthkao@gmail.com>
2017-04-07 14:50:26 -0700

    Remove unecessary shebang lines. (#181)
    * Remove unecessary shebang lines.

--- mobly/suite_runner.py
+++ mobly/suite_runner.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3.4
-#
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- tests/mobly/controllers/android_device_lib/jsonrpc_client_base_test.py
+++ tests/mobly/controllers/android_device_lib/jsonrpc_client_base_test.py
@@ -1,5 +1,3 @@
-#/usr/bin/env python3.4
-#
 # Copyright 2016 Google Inc.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
