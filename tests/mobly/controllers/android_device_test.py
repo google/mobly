cd2f4388d3779e376d420354325428d32e0576eb
Ang Li <angli@google.com>
2017-05-03 17:08:09 -0700

    Fix unit test.

--- tests/mobly/controllers/android_device_test.py
+++ tests/mobly/controllers/android_device_test.py
@@ -280,10 +280,9 @@ class AndroidDeviceTest(unittest.TestCase):
                                          "AndroidDevice%s" % ad.serial,
                                          "adblog,fakemodel,%s.txt" % ad.serial)
         creat_dir_mock.assert_called_with(os.path.dirname(expected_log_path))
-        adb_cmd = 'adb -s %s logcat -v threadtime -b all >> %s'
+        adb_cmd = 'adb -s %s logcat -v threadtime  >> %s'
         start_proc_mock.assert_called_with(
-              adb_cmd % (ad.serial, expected_log_path), check_health_delay=0.1,
-                         shell=True)
+              adb_cmd % (ad.serial, expected_log_path), shell=True)
         self.assertEqual(ad.adb_logcat_file_path, expected_log_path)
         expected_msg = ('Logcat thread is already running, cannot start another'
                         ' one.')
@@ -327,8 +326,7 @@ class AndroidDeviceTest(unittest.TestCase):
         creat_dir_mock.assert_called_with(os.path.dirname(expected_log_path))
         adb_cmd = 'adb -s %s logcat -v threadtime -b radio >> %s'
         start_proc_mock.assert_called_with(
-              adb_cmd % (ad.serial, expected_log_path), check_health_delay=0.1,
-                         shell=True)
+              adb_cmd % (ad.serial, expected_log_path), shell=True)
         self.assertEqual(ad.adb_logcat_file_path, expected_log_path)
 
     @mock.patch('mobly.controllers.android_device_lib.adb.AdbProxy', return_value=mock_android_device.MockAdbProxy(1))
