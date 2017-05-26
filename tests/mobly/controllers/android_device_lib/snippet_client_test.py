af67dbf1e83ae73d7d5a996fb563548b25f1d1de
Alexander Dorokhine <adorokhine@google.com>
2017-05-26 14:19:29 -0700

    Add unit tests for starting snippets.

--- tests/mobly/controllers/android_device_lib/snippet_client_test.py
+++ tests/mobly/controllers/android_device_lib/snippet_client_test.py
@@ -20,10 +20,11 @@ import unittest
 
 from mobly.controllers.android_device_lib import jsonrpc_client_base
 from mobly.controllers.android_device_lib import snippet_client
+from tests.lib import jsonrpc_client_test_base
 
 MOCK_PACKAGE_NAME = 'some.package.name'
 MOCK_MISSING_PACKAGE_NAME = 'not.installed'
-JSONRPC_BASE_PACKAGE = 'mobly.controllers.android_device_lib.jsonrpc_client_base.JsonRpcClientBase'
+JSONRPC_BASE_CLASS = 'mobly.controllers.android_device_lib.jsonrpc_client_base.JsonRpcClientBase'
 
 
 class MockAdbProxy(object):
@@ -63,46 +64,55 @@ class MockAdbProxy(object):
         return adb_call
 
 
-class JsonRpcClientBaseTest(unittest.TestCase):
+class SnippetClientTest(jsonrpc_client_test_base.JsonRpcClientTestBase):
     """Unit tests for mobly.controllers.android_device_lib.snippet_client.
     """
 
-    @mock.patch('socket.create_connection')
-    @mock.patch(JSONRPC_BASE_PACKAGE)
-    def test_check_app_installed_normal(self, mock_create_connection,
-                                        mock_client_base):
+    def test_check_app_installed_normal(self):
         sc = self._make_client()
         sc._check_app_installed()
 
-    @mock.patch('socket.create_connection')
-    @mock.patch(JSONRPC_BASE_PACKAGE)
-    def test_check_app_installed_fail_app_not_installed(
-            self, mock_create_connection, mock_client_base):
+    def test_check_app_installed_fail_app_not_installed(self):
         sc = self._make_client(MockAdbProxy(apk_not_installed=True))
         expected_msg = '%s is not installed on .*' % MOCK_PACKAGE_NAME
         with self.assertRaisesRegexp(jsonrpc_client_base.AppStartError,
                                      expected_msg):
             sc._check_app_installed()
 
-    @mock.patch('socket.create_connection')
-    @mock.patch(JSONRPC_BASE_PACKAGE)
-    def test_check_app_installed_fail_not_instrumented(
-            self, mock_create_connection, mock_client_base):
+    def test_check_app_installed_fail_not_instrumented(self):
         sc = self._make_client(MockAdbProxy(apk_not_instrumented=True))
         expected_msg = '%s is installed on .*, but it is not instrumented.' % MOCK_PACKAGE_NAME
         with self.assertRaisesRegexp(jsonrpc_client_base.AppStartError,
                                      expected_msg):
             sc._check_app_installed()
 
-    @mock.patch('socket.create_connection')
-    @mock.patch(JSONRPC_BASE_PACKAGE)
-    def test_check_app_installed_fail_target_not_installed(
-            self, mock_create_connection, mock_client_base):
+    def test_check_app_installed_fail_target_not_installed(self):
         sc = self._make_client(MockAdbProxy(target_not_installed=True))
         expected_msg = 'Instrumentation target %s is not installed on .*' % MOCK_MISSING_PACKAGE_NAME
         with self.assertRaisesRegexp(jsonrpc_client_base.AppStartError,
                                      expected_msg):
             sc._check_app_installed()
+
+    @mock.patch('socket.create_connection')
+    def test_snippet_start(self, mock_create_connection):
+        import pudb
+        pudb.set_trace()
+        self.setup_mock_socket_file(mock_create_connection)
+        client = self._make_client()
+        client.connect()
+        result = client.testSnippetCall()
+        self.assertEqual(123, result)
+
+    @mock.patch('socket.create_connection')
+    def test_snippet_start_event_client(self, mock_create_connection):
+        import pudb
+        pudb.set_trace()
+        fake_file = self.setup_mock_socket_file(mock_create_connection)
+        client = self._make_client()
+        client.connect()
+        fake_file.resp = self.MOCK_RESP_WITH_CALLBACK
+        result = client.testSnippetCall()
+        self.assertEqual(123, result)
 
     def _make_client(self, adb_proxy=MockAdbProxy()):
         return snippet_client.SnippetClient(
