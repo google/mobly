60be8dc31430bf85aba21f789554480678e9e206
Keith Dart <keith.dart@gmail.com>
2017-06-15 18:11:18 -0700

    Fix some style errors. (#230)
    Extraneous whitespace and mutable initializer variables.

--- mobly/base_test.py
+++ mobly/base_test.py
@@ -1,11 +1,11 @@
 # Copyright 2016 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -97,8 +97,8 @@ class BaseTestClass(object):
         self._safe_exec_func(self.clean_up)
 
     def unpack_userparams(self,
-                          req_param_names=[],
-                          opt_param_names=[],
+                          req_param_names=None,
+                          opt_param_names=None,
                           **kwargs):
         """An optional function that unpacks user defined parameters into
         individual variables.
@@ -126,6 +126,8 @@ class BaseTestClass(object):
         Raises:
             Error is raised if a required user params is not provided.
         """
+        req_param_names = req_param_names or []
+        opt_param_names = opt_param_names or []
         for k, v in kwargs.items():
             if k in self.user_params:
                 v = self.user_params[k]
