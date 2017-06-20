1f6554595f6dc6a72c77e7ec06c6d294d61344cf
lmeng <lmeng@google.com>
2017-06-19 17:14:15 -0700

    Fix comments

--- mobly/controllers/android_device_lib/sl4a_client.py
+++ mobly/controllers/android_device_lib/sl4a_client.py
@@ -100,14 +100,10 @@ class Sl4aClient(jsonrpc_client_base.JsonRpcClientBase):
             # Failed to connect to app (could because of reboot). Do not restart
             # app, instead, raise exception. Because this is not expected for
             # re-connect.
-            try:
-                self.stop_app()
-            except Exception as e:
-                # Failed to connect to app, something went wrong.
-                raise jsonrpc_client_base.AppRestoreConnectionError(
-                    ('Failed to restore app connection for %s at host port %s, '
-                     'device port %s'),
-                    self.package, self.host_port, self.device_port)
+            raise jsonrpc_client_base.AppRestoreConnectionError(
+                ('Failed to restore app connection for %s at host port %s, '
+                 'device port %s'),
+                self.package, self.host_port, self.device_port)
         self.ed = self._start_event_client()
 
     def stop_app(self):
