1493710b2fa167349303bc6710dda042dae0b895
Ang Li <angli@google.com>
2017-08-22 10:58:27 -0700

    Mobly Release 1.6.1 (#319)

--- setup.py
+++ setup.py
@@ -59,13 +59,13 @@ class PyTest(test.test):
 def main():
     setuptools.setup(
         name='mobly',
-        version='1.5',
+        version='1.6.1',
         maintainer = 'Ang Li',
         maintainer_email = 'mobly-github@googlegroups.com',
         description='Automation framework for special end-to-end test cases',
         license='Apache2.0',
         url = 'https://github.com/google/mobly',
-        download_url = 'https://github.com/google/mobly/tarball/1.5',
+        download_url = 'https://github.com/google/mobly/tarball/1.6.1',
         packages=setuptools.find_packages(),
         include_package_data=False,
         scripts=['tools/sl4a_shell.py', 'tools/snippet_shell.py'],
