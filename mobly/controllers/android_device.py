41979c89e8b741071f4d90fe81cc0f8724c9dec1
Ang Li <angli@google.com>
2017-09-01 13:58:47 -0700

    Escape spaces in logcat file names for cli inputs.

--- mobly/controllers/android_device.py
+++ mobly/controllers/android_device.py
@@ -836,10 +836,12 @@ class AndroidDevice(object):
             extra_params = self.adb_logcat_param
         except AttributeError:
             extra_params = ''
-        cmd = '"%s" -s %s logcat -v threadtime %s >> %s' % (adb.ADB,
-                                                            self.serial,
-                                                            extra_params,
-                                                            logcat_file_path)
+        cmd = '"%s" -s %s logcat -v threadtime %s >> %s' % (
+            adb.ADB,
+            self.serial,
+            extra_params,
+            # Only need to escape space here as it's a raw cli string for `>>`.
+            logcat_file_path.replace(' ', r'\ '))
         process = utils.start_standing_subprocess(cmd, shell=True)
         self._adb_logcat_process = process
         self.adb_logcat_file_path = logcat_file_path
