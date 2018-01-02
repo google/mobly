72c120a47b873fb0be0f470f1ba0bf2d98edfdea
Ang Li <angli@google.com>
2018-01-02 13:21:09 -0800

    Support adding extra info in test summary.

--- mobly/base_test.py
+++ mobly/base_test.py
@@ -334,6 +334,22 @@ class BaseTestClass(object):
                               func.__name__, self.current_test_name)
             tr_record.add_error(func.__name__, e)
 
+    def add_to_summary(self, content):
+        """Add a USER_CONTENT entry to test summary file.
+
+        Sometimes additional data need to be recorded in summary file for
+        debugging or post-test analysis.
+
+        Each call adds a new entry to the summary file, with no guarantee of
+        its position among the summary file entries.
+
+        Args:
+            content: dict, the data to add to summary file.
+        """
+        if 'timestamp' not in content:
+            content['timestamp'] = time.time()
+        self.summary_writer.dump(content)
+
     def exec_one_test(self, test_name, test_method, args=(), **kwargs):
         """Executes one test and update test results.
 
