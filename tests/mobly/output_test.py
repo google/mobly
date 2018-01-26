36de569371aaefd69a2335d5f8c77855a142eaed
winterfrosts <winterfrosts@google.com>
2018-01-25 19:01:02 -0800

    Allow logging to start before running tests

--- tests/mobly/output_test.py
+++ tests/mobly/output_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import os
 import shutil
 import tempfile
@@ -25,6 +26,20 @@ from mobly import test_runner
 from tests.lib import mock_controller
 from tests.lib import integration_test
 from tests.lib import teardown_class_failure_test
+
+
+def create_mock_test_config(base_mock_test_config):
+    mock_test_config = base_mock_test_config.copy()
+    mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
+    my_config = [{
+        'serial': 'xxxx',
+        'magic': 'Magic1'
+    }, {
+        'serial': 'xxxx',
+        'magic': 'Magic2'
+    }]
+    mock_test_config.controller_configs[mock_ctrlr_config_name] = my_config
+    return mock_test_config
 
 
 class OutputTest(unittest.TestCase):
@@ -48,22 +63,46 @@ class OutputTest(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.tmp_dir)
 
+    def test_setup_logger_before_run(self):
+        """Verifies the expected output files from a test run.
+
+        * Files are correctly created.
+        * Basic sanity checks of each output file.
+        """
+        mock_test_config = create_mock_test_config(self.base_mock_test_config)
+        info_uuid = 'e098d4ff-4e90-4e08-b369-aa84a7ef90ec'
+        debug_uuid = 'c6f1474e-960a-4df8-8305-1c5b8b905eca'
+        tr = test_runner.TestRunner(self.log_dir, self.test_bed_name)
+        tr.setup_logger()
+        logging.info(info_uuid)
+        logging.debug(debug_uuid)
+        tr.add_test_class(mock_test_config, integration_test.IntegrationTest)
+        tr.run()
+        output_dir = os.path.join(self.log_dir, self.test_bed_name, 'latest')
+        summary_file_path = os.path.join(output_dir,
+                                         records.OUTPUT_FILE_SUMMARY)
+        debug_log_path = os.path.join(output_dir,
+                                      records.OUTPUT_FILE_DEBUG_LOG)
+        info_log_path = os.path.join(output_dir, records.OUTPUT_FILE_INFO_LOG)
+        self.assertTrue(os.path.isfile(summary_file_path))
+        self.assertTrue(os.path.isfile(debug_log_path))
+        self.assertTrue(os.path.isfile(info_log_path))
+        with open(debug_log_path, 'r') as f:
+            content = f.read()
+            self.assertIn(debug_uuid, content)
+            self.assertIn(info_uuid, content)
+        with open(info_log_path, 'r') as f:
+            content = f.read()
+            self.assertIn(info_uuid, content)
+            self.assertNotIn(debug_uuid, content)
+
     def test_basic_output(self):
         """Verifies the expected output files from a test run.
 
         * Files are correctly created.
         * Basic sanity checks of each output file.
         """
-        mock_test_config = self.base_mock_test_config.copy()
-        mock_ctrlr_config_name = mock_controller.MOBLY_CONTROLLER_CONFIG_NAME
-        my_config = [{
-            'serial': 'xxxx',
-            'magic': 'Magic1'
-        }, {
-            'serial': 'xxxx',
-            'magic': 'Magic2'
-        }]
-        mock_test_config.controller_configs[mock_ctrlr_config_name] = my_config
+        mock_test_config = create_mock_test_config(self.base_mock_test_config)
         tr = test_runner.TestRunner(self.log_dir, self.test_bed_name)
         tr.add_test_class(mock_test_config, integration_test.IntegrationTest)
         tr.run()
