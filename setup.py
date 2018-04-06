414f0c6adc3afbe3519b45e7a3f7873f36c3b1c3
Ang Li <angli@google.com>
2018-04-06 16:25:00 -0700

    Mobly Release 1.7.2.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # Copyright 2016 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,19 +60,20 @@ class PyTest(test.test):
 def main():
     setuptools.setup(
         name='mobly',
-        version='1.7.1',
-        maintainer = 'Ang Li',
-        maintainer_email = 'mobly-github@googlegroups.com',
+        version='1.7.2',
+        maintainer='Ang Li',
+        maintainer_email='mobly-github@googlegroups.com',
         description='Automation framework for special end-to-end test cases',
         license='Apache2.0',
-        url = 'https://github.com/google/mobly',
-        download_url = 'https://github.com/google/mobly/tarball/1.7.1',
+        url='https://github.com/google/mobly',
+        download_url='https://github.com/google/mobly/tarball/1.7.2',
         packages=setuptools.find_packages(),
         include_package_data=False,
         scripts=['tools/sl4a_shell.py', 'tools/snippet_shell.py'],
         tests_require=['pytest'],
         install_requires=install_requires,
-        cmdclass={'test': PyTest}, )
+        cmdclass={'test': PyTest},
+    )
 
 
 if __name__ == '__main__':
