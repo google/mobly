6202983ea107f458af834fd9a11dcd1b443f432e
Sparky Zhang <winterfrosts@google.com>
2018-04-11 19:19:19 -0700

    Yapf precommit didn't trigger.

--- tests/mobly/logger_test.py
+++ tests/mobly/logger_test.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
 import unittest
 import pytz
-import mock
 
 from mobly import logger
 
