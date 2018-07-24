53074839c7c9587eb467213feeb6bbde40a04a15
hyg <hyg@webterren.com>
2018-07-24 17:18:54 +0800

    use normalized_serial as property

--- mobly/controllers/android_device.py
+++ mobly/controllers/android_device.py
@@ -429,7 +429,8 @@ class AndroidDevice(object):
             via fastboot.
     """
 
-    def _normalized_serial(self):
+    @property
+    def normalized_serial(self):
         """Sanitized serial name for usage in log filename.
 
         Some Android emulators use ip:port as their serial names, while on Windows `:` is not
@@ -437,16 +438,16 @@ class AndroidDevice(object):
         """
         if self._serial is None:
             return None
-        n = self._serial.replace(' ', '_')
-        n = n.replace(':', '-')
-        return n
+        normalized_serial = self._serial.replace(' ', '_')
+        normalized_serial = normalized_serial.replace(':', '-')
+        return normalized_serial
 
     def __init__(self, serial=''):
         self._serial = str(serial)
         # logging.log_path only exists when this is used in an Mobly test run.
         self._log_path_base = getattr(logging, 'log_path', '/tmp/logs')
         self._log_path = os.path.join(self._log_path_base,
-                                      'AndroidDevice%s' % self._normalized_serial())
+                                      'AndroidDevice%s' % self.normalized_serial)
         self._debug_tag = self._serial
         self.log = AndroidDeviceLoggerAdapter(logging.getLogger(), {
             'tag': self.debug_tag
@@ -1037,7 +1038,7 @@ class AndroidDevice(object):
 
         self._enable_logpersist()
 
-        f_name = 'adblog,%s,%s.txt' % (self.model, self._normalized_serial())
+        f_name = 'adblog,%s,%s.txt' % (self.model, self.normalized_serial)
         utils.create_dir(self.log_path)
         logcat_file_path = os.path.join(self.log_path, f_name)
         try:
@@ -1092,7 +1093,7 @@ class AndroidDevice(object):
         else:
             br_path = os.path.join(self.log_path, 'BugReports')
         utils.create_dir(br_path)
-        base_name = ',%s,%s.txt' % (begin_time, self._normalized_serial())
+        base_name = ',%s,%s.txt' % (begin_time, self.normalized_serial)
         if new_br:
             base_name = base_name.replace('.txt', '.zip')
         test_name_len = utils.MAX_FILENAME_LEN - len(base_name)
