425a6501a150f3f3821fba073b97b662a85022bf
Ang Li <angli@google.com>
2018-09-26 11:07:52 -0700

    Introduce proper service management for AndroidDevice. (#503)
    This is the initial check in of AndroidDevice service mechanism.
    More changes and refactorings are coming once the first service (logcat) is in.
    
    * A proper way to manage the life cycles of the AndroidDevice's services.
    * Add the service manager into `AndroidDevice`.
    * Implement the service for logcat.

---
+++ mobly/controllers/android_device_lib/service_manager.py
@@ -0,0 +1,112 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for the manager of services."""
+# TODO(xpconanfan: move the device errors to a more generic location so
+# other device controllers like iOS can share it.
+from mobly import expects
+from mobly.controllers.android_device_lib import errors
+
+
+class Error(errors.DeviceError):
+    """Root error type for this module."""
+
+
+class ServiceManager(object):
+    """Manager for services of AndroidDevice.
+
+    A service is a long running process that involves an Android device, like
+    adb logcat or Snippet.
+    """
+
+    def __init__(self, device):
+        self._service_objects = {}
+        self._device = device
+
+    @property
+    def is_any_alive(self):
+        """True if any service is alive; False otherwise."""
+        for service in self._service_objects.values():
+            if service.is_alive:
+                return True
+        return False
+
+    def register(self, alias, service_class, configs=None):
+        """Registers a service.
+
+        This will create a service instance, starts the service, and adds the
+        instance to the mananger.
+
+        Args:
+            alias: string, the alias for this instance.
+            service_class: class, the service class to instantiate.
+            configs: (optional) config object to pass to the service class's
+                constructor.
+        """
+        if alias in self._service_objects:
+            raise Error(
+                self._device,
+                'A service is already registered with alias "%s".' % alias)
+        service_obj = service_class(self._device, configs)
+        service_obj.start()
+        self._service_objects[alias] = service_obj
+
+    def unregister(self, alias):
+        """Unregisters a service instance.
+
+        Stops a service and removes it from the manager.
+
+        Args:
+            alias: string, the alias of the service instance to unregister.
+        """
+        if alias not in self._service_objects:
+            raise Error(self._device,
+                        'No service is registered with alias "%s".' % alias)
+        service_obj = self._service_objects.pop(alias)
+        if service_obj.is_alive:
+            with expects.expect_no_raises(
+                    'Failed to stop service instance "%s".' % alias):
+                service_obj.stop()
+
+    def unregister_all(self):
+        """Safely unregisters all active instances.
+
+        Errors occurred here will be recorded but not raised.
+        """
+        aliases = list(self._service_objects.keys())
+        for alias in aliases:
+            self.unregister(alias)
+
+    def pause_all(self):
+        """Pauses all active service instances."""
+        for alias, obj in self._service_objects.items():
+            if obj.is_alive:
+                with expects.expect_no_raises(
+                        'Failed to pause service "%s".' % alias):
+                    obj.pause()
+
+    def resume_all(self):
+        """Resumes all paused service instances."""
+        for alias, obj in self._service_objects.items():
+            if not obj.is_alive:
+                with expects.expect_no_raises(
+                        'Failed to pause service "%s".' % alias):
+                    obj.resume()
+
+    def __getattr__(self, name):
+        """Syntactic sugar to enable direct access of service objects by alias.
+
+        Args:
+            name: string, the alias a service object was registered under.
+        """
+        return self._service_objects[name]
---
+++ mobly/controllers/android_device_lib/services/base_service.py
@@ -0,0 +1,87 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for the BaseService."""
+
+
+class BaseService(object):
+    """Base class of a Mobly AndroidDevice service.
+
+    This class defines the interface for Mobly's AndroidDevice service.
+    """
+
+    def __init__(self, device, configs=None):
+        """Constructor of the class.
+
+        Args:
+          device: the device object this service is associated with.
+          config: optional configuration defined by the author of the service
+              class.
+        """
+        self._device = device
+        self._configs = configs
+
+    @property
+    def is_alive(self):
+        """True if the service is active; False otherwise."""
+        raise NotImplementedError('"is_alive" is a required service property.')
+
+    def start(self, configs=None):
+        """Starts the service.
+
+        Args:
+            configs: optional configs to be passed for startup.
+        """
+        raise NotImplementedError('"start" is a required service method.')
+
+    def stop(self):
+        """Stops the service and cleans up all resources.
+
+        This method should handle any error and not throw.
+        """
+        raise NotImplementedError('"stop" is a required service method.')
+
+    def pause(self):
+        """Pauses a service temporarily.
+
+        For when the Python service object needs to temporarily lose connection
+        to the device without shutting down the service running on the actual
+        device.
+
+        This is relevant when a service needs to maintain a constant connection
+        to the device and the connection is lost if USB connection to the
+        device is disrupted.
+
+        E.g. a services that utilizes a socket connection over adb port
+        forwarding would need to implement this for the situation where the USB
+        connection to the device will be temporarily cut, but the device is not
+        rebooted.
+
+        For more context, see:
+        `mobly.controllers.android_device.AndroidDevice.handle_usb_disconnect`
+
+        If not implemented, we assume the service is not sensitive to device
+        disconnect, and `stop` will be called by default.
+        """
+        self.stop()
+
+    def resume(self):
+        """Resumes a paused service.
+
+        Same context as the `pause` method. This should resume the service
+        after the connection to the device has been re-established.
+
+        If not implemented, we assume the service is not sensitive to device
+        disconnect, and `start` will be called by default.
+        """
+        self.start(configs=self._configs)
