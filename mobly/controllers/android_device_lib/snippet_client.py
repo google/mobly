96a5add16f47e425c8c2123de177ff3f97fd4ed0
Ang Li <angli@google.com>
2018-10-01 19:06:23 -0700

    Fix pause/resume behavior for snippet management service. (#513)
    * Pause/Resume should not rely on is_alive
    * Clean up adb forward port when pausing snippet clients.

--- mobly/controllers/android_device_lib/snippet_client.py
+++ mobly/controllers/android_device_lib/snippet_client.py
@@ -236,8 +236,14 @@ class SnippetClient(jsonrpc_client_base.JsonRpcClientBase):
                     'Failed to stop existing apk. Unexpected output: %s' % out)
         finally:
             # Always clean up the adb port
-            if self.host_port:
-                self._adb.forward(['--remove', 'tcp:%d' % self.host_port])
+            self.clear_host_port()
+
+    def clear_host_port(self):
+        """Stops the adb port forwarding of the host port used by this client.
+        """
+        if self.host_port:
+            self._adb.forward(['--remove', 'tcp:%d' % self.host_port])
+            self.host_port = None
 
     def _start_event_client(self):
         """Overrides superclass."""
