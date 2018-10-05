1b3106461050a26c9497c2ca85a758d4da2b1625
Ang Li <angli@google.com>
2018-10-05 10:59:28 -0700

    Fix load_sl4a. (#522)

--- tests/mobly/controllers/android_device_lib/services/sl4a_service_test.py
+++ tests/mobly/controllers/android_device_lib/services/sl4a_service_test.py
@@ -15,6 +15,7 @@ import mock
 from future.tests.base import unittest
 
 from mobly.controllers.android_device_lib.services import sl4a_service
+from mobly.controllers.android_device_lib import service_manager
 
 
 @mock.patch('mobly.controllers.android_device_lib.sl4a_client.Sl4aClient')
@@ -25,36 +26,42 @@ class Sl4aServiceTest(unittest.TestCase):
         service = sl4a_service.Sl4aService(mock.MagicMock())
         self.assertFalse(service.is_alive)
 
-    def test_start(self, mock_class):
-        mock_client = mock_class.return_value
+    def test_start(self, mock_sl4a_client_class):
+        mock_client = mock_sl4a_client_class.return_value
         service = sl4a_service.Sl4aService(mock.MagicMock())
         service.start()
         mock_client.start_app_and_connect.assert_called_once_with()
         self.assertTrue(service.is_alive)
 
-    def test_stop(self, mock_class):
-        mock_client = mock_class.return_value
+    def test_stop(self, mock_sl4a_client_class):
+        mock_client = mock_sl4a_client_class.return_value
         service = sl4a_service.Sl4aService(mock.MagicMock())
         service.start()
         service.stop()
         mock_client.stop_app.assert_called_once_with()
         self.assertFalse(service.is_alive)
 
-    def test_pause(self, mock_class):
-        mock_client = mock_class.return_value
+    def test_pause(self, mock_sl4a_client_class):
+        mock_client = mock_sl4a_client_class.return_value
         service = sl4a_service.Sl4aService(mock.MagicMock())
         service.start()
         service.pause()
         mock_client.stop_event_dispatcher.assert_called_once_with()
         mock_client.clear_host_port.assert_called_once_with()
 
-    def test_resume(self, mock_class):
-        mock_client = mock_class.return_value
+    def test_resume(self, mock_sl4a_client_class):
+        mock_client = mock_sl4a_client_class.return_value
         service = sl4a_service.Sl4aService(mock.MagicMock())
         service.start()
         service.pause()
         service.resume()
         mock_client.restore_app_connection.assert_called_once_with()
+
+    def test_register_with_service_manager(self, _):
+        mock_device = mock.MagicMock()
+        manager = service_manager.ServiceManager(mock_device)
+        manager.register('sl4a', sl4a_service.Sl4aService)
+        self.assertTrue(manager.sl4a)
 
 
 if __name__ == '__main__':
