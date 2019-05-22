c142305c00440e682a0eb30ab7b3aff146dd0155
Jeff Herman <jeffherman@google.com>
2019-05-21 20:44:52 -0700

    Remove sys.path modification and add sphinx.ext.autosummary.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,6 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 
 # -- General configuration ------------------------------------------------
@@ -30,7 +29,9 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode']
