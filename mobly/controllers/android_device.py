85fde55f940f45ff60e11491d7de65e6cb7527dc
jamesgkk <79961152+jamesgkk@users.noreply.github.com>
2021-05-05 15:22:30 +0800

    Let user to change the prefix file name of screenshot

--- mobly/controllers/android_device.py
+++ mobly/controllers/android_device.py
@@ -995,19 +995,17 @@ class AndroidDevice:
     self.log.debug('Bugreport taken at %s.', full_out_path)
     return full_out_path
 
-  def take_screenshot(self, destination, filename=None):
+  def take_screenshot(self, destination, filename='screenshot'):
     """Takes a screenshot of the device.
 
     Args:
       destination: string, full path to the directory to save in.
-      filename: string, additional name of the screenshot will be added if given.
+      filename: string, prefix file name of the screenshot.
 
     Returns:
       string, full path to the screenshot file on the host.
     """
-    prefix = 'screenshot'
-    if filename:
-        prefix = f'screenshot_{filename}'
+    prefix = filename
     filename = self.generate_filename(prefix, extension_name='png')
     device_path = os.path.join('/storage/emulated/0/', filename)
     self.adb.shell(['screencap', '-p', device_path],
