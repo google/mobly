b104346d399e4195dd64216794d767d6061da2a7
johnkclee <puremonkey2001@yahoo.com.tw>
2021-05-06 08:48:00 +0800

    Restore E741 flake8 rule

--- mobly/records.py
+++ mobly/records.py
@@ -463,8 +463,8 @@ class TestResultRecord:
 
   def __str__(self):
     d = self.to_dict()
-    l = ['%s = %s' % (k, v) for k, v in d.items()]
-    s = ', '.join(l)
+    kv_pairs = ['%s = %s' % (k, v) for k, v in d.items()]
+    s = ', '.join(kv_pairs)
     return s
 
   def __repr__(self):
@@ -638,9 +638,9 @@ class TestResult:
     Returns:
       A summary string of this test result.
     """
-    l = ['%s %d' % (k, v) for k, v in self.summary_dict().items()]
+    kv_pairs = ['%s %d' % (k, v) for k, v in self.summary_dict().items()]
     # Sort the list so the order is the same every time.
-    msg = ', '.join(sorted(l))
+    msg = ', '.join(sorted(kv_pairs))
     return msg
 
   def summary_dict(self):
