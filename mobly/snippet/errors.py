0bdab787903eba72f26d19967d2c5ba378689837
Minghao Li <minghaoli@google.com>
2022-03-08 15:32:21 +0800

    yapf

--- mobly/snippet/errors.py
+++ mobly/snippet/errors.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Module for errors thrown from Snippet Client objects."""
 # TODO(mhaoli): Package `mobly.snippet` should not import errors from
 # android_device_lib. However all the errors thrown from snippet clients
