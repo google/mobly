7d4560799ec5c0374d9b34d575d5c627b42ed471
Minghao Li <minghaoli@google.com>
2022-06-08 22:24:18 +0800

    before stage doesn't call stop

--- mobly/snippet/client_base.py
+++ mobly/snippet/client_base.py
@@ -101,8 +101,8 @@ class ClientBase(abc.ABC):
       2. starting the snippet server on the remote device.
       3. making a connection to the snippet server.
 
-    If error occurs at any stage, this function will abort the initialization
-    process and call `stop` to clean up.
+    An error occurs at any stage will abort the initialization. Only errors in
+    the second and third stages will trigger `stop` to clean up.
 
     Raises:
       errors.ProtocolError: something went wrong when exchanging data with the
@@ -119,11 +119,10 @@ class ClientBase(abc.ABC):
     self.log.info('Initializing the snippet package %s.', self.package)
     start_time = time.perf_counter()
 
-    try:
-      self.log.debug('Preparing to start the snippet server of %s.',
-                     self.package)
-      self.before_starting_server()
+    self.log.debug('Preparing to start the snippet server of %s.', self.package)
+    self.before_starting_server()
 
+    try:
       self.log.debug('Starting the snippet server of %s.', self.package)
       self.start_server()
 
