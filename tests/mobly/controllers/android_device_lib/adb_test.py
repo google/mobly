babbba05387687ce2456991812c5b449bfbf373b
Sachin Sagadevan <sachinss@google.com>
2023-10-25 15:11:17 +0000

    updating tests based on review comments

--- tests/mobly/controllers/android_device_lib/adb_test.py
+++ tests/mobly/controllers/android_device_lib/adb_test.py
@@ -627,19 +627,21 @@ class AdbTest(unittest.TestCase):
 
   def test_forward(self):
     with mock.patch.object(adb.AdbProxy, '_exec_cmd') as mock_exec_cmd:
-      adb.AdbProxy().forward(MOCK_SHELL_COMMAND)
-      mock_exec_cmd.assert_called_with(['adb', 'forward', 'ls'],
-                                       shell=False,
-                                       timeout=None,
-                                       stderr=None)
+      adb.AdbProxy().forward(['tcp:12345', 'tcp:98765'])
+      mock_exec_cmd.assert_called_with(
+          ['adb', 'forward', 'tcp:12345', 'tcp:98765'],
+          shell=False,
+          timeout=None,
+          stderr=None)
 
   def test_reverse(self):
     with mock.patch.object(adb.AdbProxy, '_exec_cmd') as mock_exec_cmd:
-      adb.AdbProxy().reverse(MOCK_SHELL_COMMAND)
-      mock_exec_cmd.assert_called_with(['adb', 'reverse', 'ls'],
-                                       shell=False,
-                                       timeout=None,
-                                       stderr=None)
+      adb.AdbProxy().reverse(['tcp:12345', 'tcp:98765'])
+      mock_exec_cmd.assert_called_with(
+          ['adb', 'reverse', 'tcp:12345', 'tcp:98765'],
+          shell=False,
+          timeout=None,
+          stderr=None)
 
   def test_instrument_without_parameters(self):
     """Verifies the AndroidDevice object's instrument command is correct in
