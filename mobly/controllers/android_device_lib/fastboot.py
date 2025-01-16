4aac25b7565be885a22f693ce9a565894242ce95
Scott Chen <scottlschen@google.com>
2025-01-16 09:10:51 +0800

    Add timeout support for FastbootProxy
    TEST=unit tests with tox

--- mobly/controllers/android_device_lib/fastboot.py
+++ mobly/controllers/android_device_lib/fastboot.py
@@ -13,40 +13,9 @@
 # limitations under the License.
 
 import logging
-from subprocess import Popen, PIPE
+from subprocess import PIPE
 
 from mobly import utils
-
-
-def exe_cmd(*cmds):
-  """Executes commands in a new shell. Directing stderr to PIPE.
-
-  This is fastboot's own exe_cmd because of its peculiar way of writing
-  non-error info to stderr.
-
-  Args:
-    cmds: A sequence of commands and arguments.
-
-  Returns:
-    The output of the command run.
-
-  Raises:
-    Exception: An error occurred during the command execution.
-  """
-  cmd = ' '.join(cmds)
-  proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-  (out, err) = proc.communicate()
-  ret = proc.returncode
-  logging.debug(
-      'cmd: %s, stdout: %s, stderr: %s, ret: %s',
-      utils.cli_cmd_to_string(cmds),
-      out,
-      err,
-      ret,
-  )
-  if not err:
-    return out
-  return err
 
 
 class FastbootProxy:
@@ -58,19 +27,53 @@ class FastbootProxy:
   >> fb.devices() # will return the console output of "fastboot devices".
   """
 
-  def __init__(self, serial=''):
+  def __init__(self, serial='', timeout = 1200):
     self.serial = serial
+    self._timeout = timeout
 
   def fastboot_str(self):
     if self.serial:
       return 'fastboot -s {}'.format(self.serial)
     return 'fastboot'
 
+  def _exec_cmd_timeout(self, *cmds):
+    """Executes commands in a new shell. Directing stderr to PIPE, with timeout.
+
+    This is fastboot's own exec_cmd function because of its peculiar way of
+    writing non-error info to stderr.
+
+    Args:
+      *cmds: The commands to execute.
+
+    Returns:
+      The output in bytes of the command run.
+    """
+    cmd = ' '.join(cmds)
+    (ret, out, err) = utils.run_command(
+        cmd=cmd,
+        stdout=PIPE,
+        stderr=PIPE,
+        shell=True,
+        timeout=self._timeout,
+    )
+    logging.debug(
+        'cmd: %s, stdout: %s, stderr: %s, ret: %s',
+        utils.cli_cmd_to_string(cmds),
+        out,
+        err,
+        ret,
+    )
+    if not err:
+      return out
+    return err
+
   def _exec_fastboot_cmd(self, name, arg_str):
-    return exe_cmd(' '.join((self.fastboot_str(), name, arg_str)))
+    return self._exec_cmd_timeout(
+        ' '.join((self.fastboot_str(), name, arg_str))
+    )
 
   def args(self, *args):
-    return exe_cmd(' '.join((self.fastboot_str(),) + args))
+    return self._exec_cmd_timeout(' '.join((self.fastboot_str(),) + args))
 
   def __getattr__(self, name):
     def fastboot_call(*args):
