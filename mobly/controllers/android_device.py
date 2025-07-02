7bfe05de81eb352337bbebc21921ba92bc0d69d5
rpius <116300722+rpius@users.noreply.github.com>
2025-07-02 16:25:57 -0700

    Add BUILD_VERSION_SDK_FULL to AndroidProperties (#978)
    This is the new versioning variable for minor API bumps.

--- mobly/controllers/android_device.py
+++ mobly/controllers/android_device.py
@@ -54,6 +54,7 @@ CACHED_SYSTEM_PROPS = [
     'ro.build.version.codename',
     'ro.build.version.incremental',
     'ro.build.version.sdk',
+    'ro.build.version.sdk_full',
     'ro.build.product',
     'ro.build.characteristics',
     'ro.debuggable',
@@ -481,6 +482,8 @@ class BuildInfoConstants(enum.Enum):
       'ro.build.version.incremental',
   )
   BUILD_VERSION_SDK = 'build_version_sdk', 'ro.build.version.sdk'
+  # Refer to android.os.Build.VERSION#SDK_INT_FULL (Available on `SDK_INT > 36` devices)
+  BUILD_VERSION_SDK_FULL = 'build_version_sdk_full', 'ro.build.version.sdk_full'
   BUILD_PRODUCT = 'build_product', 'ro.build.product'
   BUILD_CHARACTERISTICS = 'build_characteristics', 'ro.build.characteristics'
   DEBUGGABLE = 'debuggable', 'ro.debuggable'
