google/mobly

commit bc0dc9c44dc02b2be9607daa961129ea83ae1c68

Avoid log spam by specifying package name in look-up commands (#983)
This CL change specifies the package name in `pm list packages`/`instrumentation` to avoid all packages being printed. Because long adb command outputs usually cause log spam and slow down tests.

mobly/controllers/android_device_lib/snippet_client_v2.py
@@ -228,7 +228,9 @@ class SnippetClientV2(client_base.ClientBase):
         for the current user.
     """
     # Validate that the Mobly Snippet app is installed for the current user.
-    out = self._adb.shell(f'pm list package --user {self.user_id}')
+    out = self._adb.shell(
+        f'pm list packages --user {self.user_id} {self.package}'
+    )
     if not utils.grep(f'^package:{self.package}$', out):
       raise errors.ServerStartPreCheckError(
           self._device,
@@ -236,7 +238,7 @@ class SnippetClientV2(client_base.ClientBase):
       )
 
     # Validate that the app is instrumented.
-    out = self._adb.shell('pm list instrumentation')
+    out = self._adb.shell(f'pm list instrumentation {self.package}')
     matched_out = utils.grep(
         f'^instrumentation:{self.package}/{_INSTRUMENTATION_RUNNER_PACKAGE}',
         out,

commit 65682d343a8f585b7e119d21635f99f117c1e90a

Fix breakage caused by PR #983

mobly/controllers/android_device_lib/snippet_client_v2.py
@@ -238,7 +238,7 @@ class SnippetClientV2(client_base.ClientBase):
       )
 
     # Validate that the app is instrumented.
-    out = self._adb.shell(f'pm list instrumentation {self.package}')
+    out = self._adb.shell('pm list instrumentation')
     matched_out = utils.grep(
         f'^instrumentation:{self.package}/{_INSTRUMENTATION_RUNNER_PACKAGE}',
         out,